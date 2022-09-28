d4c5b074bfaae8dd28f4310f48a10a700677ad52
Florian Peer <fpeer21@gmail.com>
2022-09-28 23:46:54 +0200

    erster Commit mit BinarySearch

---
+++ Java/BinaerSuche/src/BinarySearch.java
@@ -0,0 +1,34 @@
+// Binary Search in Java
+
+class BinarySearch {
+    int binarySearch(char array[], int x, int low, int high) {
+
+        // Repeat until the pointers low and high meet each other
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (array[mid] == x)
+                return mid;
+
+            if (array[mid] < x)
+                low = mid + 1;
+
+            else
+                high = mid - 1;
+        }
+
+        return -1;
+    }
+
+    public static void main(String args[]) {
+        BinarySearch ob = new BinarySearch();
+        char array[] = { 'a','b','c','d','e' };
+        int n = array.length;
+        char x = 'e';
+        int result = ob.binarySearch(array, x, 0, n - 1);
+        if (result == -1)
+            System.out.println("Not found");
+        else
+            System.out.println("Element found at index " + result);
+    }
+}

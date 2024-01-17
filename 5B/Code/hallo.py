a73689f72fd1e96297666779dbb4df161f07c2ae
Florian Peer <fpeer21@gmail.com>
2024-01-17 22:54:53 +0100

    schere stein papier programmiert

--- 5B/Code/hallo.py
+++ 5B/Code/hallo.py
@@ -1,18 +1,12 @@
-index = []
-zahl = []
+# kurz was rechnen
 
+summe = 0
+
+def rechnen():
+    summe = 32 * 2 + 25 * 2
+    print(summe)
 
 if __name__ == '__main__':
-    print("hallo")
+    rechnen()
 
 
-
-
-
-
-    #man zieht den Index
-    #beim index ist jeweils eine zugehörige Zahl zugewiesen
-    #wenn man z.B. die Zahl 20 im Index zieht, wird die Zahl, die beim Index 20 steht einmal verwendet
-    #und dann wird diese Zahl mit der Letzten Zahl in der Zahlenliste getauscht
-    #beim nächsten mal Ziehen wird nicht mehr von 0 bis 45 gezählt sondern nur noch von 0 bis 44, damit
-    # die letzte Zahl (die davor getauscht wurde) nicht nocheinmal gezogen wird
